b46d3d06683660de9268cc319c9cfcd50de421d9
muntyan <i.muntyan@vipservice.rum>
2024-02-09 21:22:36 +0300

    TestRQ

--- src/main/java/featureTest/FirstTest.java
+++ src/main/java/featureTest/FirstTest.java
@@ -1,8 +1,21 @@
 package featureTest;
 
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.testng.annotations.Test;
+
+import static org.testng.TestNGAntTask.Mode.junit;
+
+@Epic("MainTest")
+@Feature("Ticket")
+@Story("OK")
+
 
 public class FirstTest {
 
-
+    @Test
+    @DisplayName("SU")
+    public void aeroflotStandardAdtChdInfTest() {
+        System.out.println("hello !");
+    }
 }
